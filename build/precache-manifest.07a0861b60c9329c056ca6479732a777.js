1168b51d5bbca255edd1f415d15ba2149642d306
Abdul Basit <42277316+basite@users.noreply.github.com>
2018-12-11 16:10:18 +0700

    Add files via upload

---
+++ build/precache-manifest.07a0861b60c9329c056ca6479732a777.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afcbb4e9ac858b15f2e1",
+    "url": "/static/js/main.afcbb4e9.chunk.js"
+  },
+  {
+    "revision": "fb1e68980d44b3d4ac79",
+    "url": "/static/js/1.fb1e6898.chunk.js"
+  },
+  {
+    "revision": "cb7feb7d14d6e0285cc84e668157a9b2",
+    "url": "/index.html"
+  }
+];
